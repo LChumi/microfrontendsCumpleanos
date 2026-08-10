cf27c1ce0b2aedd2bbc0bf9b4cfd558153b25e5d
LChumi <luischumi.9@gmail.com>
2026-08-10 17:50:26 -0500

    feat: added new application remote to microfrontends

--- projects/shell/src/app/app.routes.ts
+++ projects/shell/src/app/app.routes.ts
@@ -1,4 +1,6 @@
 import { Routes } from '@angular/router';
+import {LayoutComponent} from './layout/components/layout/layout.component';
+import {loadRemoteModule} from '@angular-architects/module-federation';
 
 export const routes: Routes = [
   {
@@ -7,5 +9,21 @@ export const routes: Routes = [
       import('./home/home.component').then(m =>
       m.HomeComponent
       ),
+  },
+  {
+    path: 'auth',
+    loadChildren: () =>
+      loadRemoteModule('mfe-auth', './routes').then(m => m.routes)
+  },
+  {
+    path: 'erp',
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'contabilidad',
+        loadChildren: () =>
+          loadRemoteModule('mfe-erp-contabilidad', './routes').then(m => m.routes)
+      }
+    ]
   }
 ];
