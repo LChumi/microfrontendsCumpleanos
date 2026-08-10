3bf1a2be1db5829cc453d88bdc14f856e91e1f02
LChumi <luischumi.9@gmail.com>
2026-08-10 18:07:47 -0500

    feat: added new new compoenntes to auth microfrontend

--- projects/mfe-auth/src/app/app.routes.ts
+++ projects/mfe-auth/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import {LoginComponent} from './login/login.component';
+import {ForgotComponent} from './forgot/forgot.component';
+import {EmpresasComponent} from './empresas/empresas.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'login', component:LoginComponent},
+  {path: 'forgotpassword', component: ForgotComponent},
+  {path: 'empresas', component: EmpresasComponent},
+  {path: '', redirectTo: 'login', pathMatch: 'full'}
+];
---
+++ projects/mfe-auth/src/app/forgot/forgot.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-forgot',
+  standalone: true,
+  imports: [],
+  templateUrl: './forgot.component.html',
+  styles: ``
+})
+export class ForgotComponent {
+
+}
