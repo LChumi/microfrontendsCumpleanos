58b9f5db58841e77bd414e8532fc10292db5789c
LChumi <luischumi.9@gmail.com>
2026-08-24 15:44:28 -0500

    refactor(mfe-pedidos): added move clasdes and update components and routes

--- projects/mfe-pedidos/src/app/app.routes.ts
+++ projects/mfe-pedidos/src/app/app.routes.ts
@@ -2,10 +2,16 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'pedido-despachos',
-    data: {breadcrumb: 'Pedido de despachos', favorite: true},
-    title: 'Gestion pedidos Despachos | Assist Web',
-    loadComponent: () =>
-      import('./features/procesos/pedidos/despacho/despacho.component').then(m => m.DespachoComponent)
+    path: 'procesos',
+    data: {breadcrumb: 'Procesos'},
+    children: [
+      {
+        path: 'despachos',
+        data: {breadcrumb: 'Despachos', favorite: true},
+        title: 'Pedidos por Despachar | Assist Web',
+        loadComponent: () =>
+          import('./features/procesos/pedidos/despacho/despacho.component').then(m => m.DespachoComponent)
+      }
+    ]
   }
 ];
