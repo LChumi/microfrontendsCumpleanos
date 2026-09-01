6de231c11cedb4b04ad57695205b012f1b7abdbe
LChumi <luischumi.9@gmail.com>
2026-09-01 09:40:25 -0500

    feat(feature): added mew view to aproved order #2

---
+++ projects/mfe-pedidos/src/app/features/procesos/reposicion/aprobar-reposicion/aprobar-reposicion.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-aprobar-reposicion',
+  standalone: true,
+  imports: [],
+  templateUrl: './aprobar-reposicion.component.html',
+  styles: ``
+})
+export class AprobarReposicionComponent {
+
+}
