782601a5f9af192e47f0e77daed5bf4f8d3a2035
LChumi <luischumi.9@gmail.com>
2026-09-12 12:34:45 -0500

    refactor: delete unused getAlmacen method

--- projects/mfe-contabilidad/src/app/core/services/almacen.service.ts
+++ projects/mfe-contabilidad/src/app/core/services/almacen.service.ts
@@ -12,10 +12,6 @@ export class AlmacenService {
   private url = environment.apiUrl + '/models'
   private http = inject(HttpClient)
 
-  getAlmacen(empresa:number, codigo: number): Observable<Almacen> {
-    return this.http.get<Almacen>(`${this.url}/almacen/get/${empresa}/${codigo}`)
-  }
-
   listAlamacenes(empresa:number): Observable<Almacen[]> {
     return this.http.get<Almacen[]>(`${this.url}/almacen/${empresa}`)
   }
